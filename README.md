cfbccf14533961ac909c761b42fd7a805c2f612f
Joshua <yuan_zhu123@hotmail.com>
2020-12-15 13:10:56 +0800

    update scripts

---
+++ README.md
@@ -0,0 +1,2 @@
+1) ifconfig to get your NIC name
+2) wifi-scanner to scan wifi AP signal strength
--- wifi-scanner.py
+++ wifi-scanner.py
@@ -3,23 +3,39 @@ import os
 
 parser = argparse.ArgumentParser(description='WiFi scanner')
 parser.add_argument('ni', type=str, help='network interface name, use `sudo ifconfig -a` to check')
+parser.add_argument('--ssid', type=str, default="*", nargs="+", help='network ssid to scan, default * to scan all')
+parser.add_argument('--min', type=float, default=-65, help='min signal quality in dBm for scanning')
 
 args = parser.parse_args()
 
 def parse_scan_results(results):
     ll = results.split("\n")
     aps = {}
+    in_cell = False
     for l in ll:
         words = l.strip().split(" ")
         if words[0] == "Cell":
             address = words[-1]
             aps[address] = {}
-        elif words[0].split(":")[0] == "Channel":
+            in_cell = True
+        elif words[0].split(":")[0] == 'Channel' and in_cell:
             channel = int(words[0].split(":")[1])
-            aps[address][channel] = {}
-        elif words[-1] == "dBm":
+            aps[address]['Channel'] = channel
+        elif words[-1] == 'dBm' and in_cell:
             signal_level = float(words[-2].split('=')[1])
-            aps[address][channel]["SignalLevel"] = signal_level
+            aps[address]['SignalLevel'] = signal_level
+            if signal_level < args.min:
+                del aps[address]
+                in_cell = False
+        elif words[0].split(":")[0] == 'ESSID' and in_cell:
+            ESSID = words[0].split(":")[1].strip()[1:-1]
+            aps[address]['ESSID'] = ESSID
+            if ESSID not in args.ssid:
+                del aps[address]
+                in_cell = False
+        elif words[0].split(":")[0] == 'Frequency' and in_cell:
+            frequency_GHz = float(words[0].split(":")[1].split(' ')[0])
+            aps[address]['Frequency'] = frequency_GHz
 
     return aps
 
